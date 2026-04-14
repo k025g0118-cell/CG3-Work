a8ac9b1c41db3a613b5f3580a1249824f3d0e7c3
watanabe <k025g0118@g.neec.ac.jp>
2026-04-14 15:49:07 +0900

    プロジェクトファイルを追加。

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+//Windowsアプリへのインポート(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	//出力ウィンドウへの文字出力
+	OutputDebugStringA("Helllo,DirectX!\n");
+
+	return 0;
+}
