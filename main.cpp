1018f8653f45d83d5feeff7caa15011f93f0c799
watanabe <k025g0118@g.neec.ac.jp>
2026-04-14 16:22:47 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	//出力ウィンドウへの文字出力
-	OutputDebugStringA("Helllo,DirectX!\n");
+	OutputDebugStringA("Hello,DirectX!\n");
 
 	return 0;
 }
